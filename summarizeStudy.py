d3d63133c369e009c3cb1e6949d79480d81f5e67
Felipe Garcia Diaz <41710468+felipeGarciaDiaz@users.noreply.github.com>
2018-09-27 17:21:10 -0400

    Update summarizeStudy.py

--- summarizeStudy.py
+++ summarizeStudy.py
@@ -1,10 +1,12 @@
+'''
+Developed By Felipe Garcia Diaz
+'''
 import wikipedia
 import time
 from docx import Document
 
 
 def run():
-    
     doc = Document()
     doc.add_heading("Study Sheet",0)
 
@@ -15,29 +17,20 @@ def run():
     studyMat = open(studyFile).readlines()
     
     for i in range(len(studyMat)):
-        
         try:
-
             if verboseMode == "y" or None:
-                
                 print(studyMat[i] + "\n" + wikipedia.summary(studyMat[i], sentences=num))
-
-            else:
-                
+            else:    
                 pass
-
             doc.add_heading(studyMat[i], level=1)
             desc = doc.add_paragraph(wikipedia.summary(studyMat[i], sentences=num))
             time.sleep(5)
-
         except:
-
             print("There was an error reading " + studyMat[i] + " Ignoring for now")
             pass
-
+        
     doc.add_page_break()
     doc.save(outputFile)
 
 if __name__ == "__main__":
-    
     run()
